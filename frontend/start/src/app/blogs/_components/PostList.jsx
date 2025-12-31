53bb6463727ae0f3cb42c6316b4774f33c20a286
horex-Pro <hosseinhorex@gmail.com>
2026-01-01 02:10:00 +0330

    render post's image

--- frontend/start/src/app/blogs/_components/PostList.jsx
+++ frontend/start/src/app/blogs/_components/PostList.jsx
@@ -1,25 +1,35 @@
+import Image from "next/image";
 import React from "react";
 
 async function PostList() {
-  await new Promise((res) =>
-    setTimeout(() => {
-      return res();
-    }, 3000)
-  );
   const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
   const {
     data: { posts },
   } = await response.json();
 
   console.log(posts);
-  return (
-    <div>
-        <h1>لیست بلاگ ها:</h1>
-      {posts.map((post) => (
-        <div>{post.title}</div>
-      ))}
+  return posts.length > 0 ? (
+    <div className="grid grid-cols-12 gap-8">
+      {posts.map((post) => {
+        return (
+          <div
+            className=" col-span-12 sm:col-span-6 lg:col-span-4 border border-secondary-100 p-2 rounded-lg "
+            key={post.id}
+          >
+            <div className="relative aspect-video overflow-hidden rounded-lg mb-6">
+              <Image
+                src={post.coverImageUrl}
+                fill
+                className=" object-cover object-center hover:scale-110 transition-all duration-300  ease-in-out"
+                alt={post.title}
+                quality={80}
+              />
+            </div>
+          </div>
+        );
+      })}
     </div>
-  );
+  ) : null;
 }
 
 export default PostList;
