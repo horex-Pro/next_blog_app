feb964792d39aa66c7c7e5b201f0ff3d73a1bc85
horex-Pro <hosseinhorex@gmail.com>
2026-02-10 23:15:01 +0330

    fix header to mobile responsive

--- frontend/src/app/(dashboard)/profile/_components/Header.jsx
+++ frontend/src/app/(dashboard)/profile/_components/Header.jsx
@@ -3,9 +3,13 @@ import Link from "next/link";
 import ButtonIcon from "@/components/ui/ButtonIcon";
 import Avatar from "@/components/ui/Avatar";
 import { useAuth } from "@/context/AuthContext";
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import SideBar from "./SideBar";
 
 function Header({}) {
   const { user, isLoading } = useAuth();
+  const { isOpenDrawer, setIsOpenDrawer } = useState(false);
   return (
     <header
       className={`bg-secondary-0 ${isLoading ? "bg-opacity-30 blur-md" : ""}`}
@@ -13,6 +17,12 @@ function Header({}) {
       <div className="flex items-center justify-between py-5 px-4 lg:px-8">
         <div className="flex items-center gap-x-3">
           <div className="flex flex-col lg:flex-row justify-start lg:items-center gap-x-2">
+            <ButtonIcon
+              onClick={() => setIsOpenDrawer(!isOpenDrawer)}
+              className="block lg:hidden border-none"
+            >
+              {isOpenDrawer ? <XMarkIcon /> : <Bars3Icon />}
+            </ButtonIcon>
             <span className="text-sm lg:text-lg font-bold text-secondary-700">
               سلام؛ {user?.name}
             </span>
@@ -27,6 +37,9 @@ function Header({}) {
               <Avatar src={user?.avatarUrl} />
             </ButtonIcon>
           </Link>
+          <Drawer open={isOpenDrawer} onClose={() => setIsOpenDrawer(false)}>
+            <SideBar onClose={() => setIsOpenDrawer(false)} />
+          </Drawer>
         </div>
       </div>
     </header>
