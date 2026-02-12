09eadde2e6e384dc67b6b4e8d3df25427ba5b9b2
horex-Pro <hosseinhorex@gmail.com>
2026-02-12 23:13:25 +0330

    develop requred functions for getting panel's data

--- frontend/src/app/(dashboard)/profile/page.jsx
+++ frontend/src/app/(dashboard)/profile/page.jsx
@@ -3,7 +3,11 @@ import React from "react";
 function ProfilePage() {
   return (
     <div>
-      <div className="grid gap-6 md:grid-cols-3 mb-8"></div>
+      <div className="grid gap-6 md:grid-cols-3 mb-8">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 }
