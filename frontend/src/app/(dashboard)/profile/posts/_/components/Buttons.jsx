7f2f1e7078953ed3a1c2a962176e2e19b964571b
horex-Pro <hosseinhorex@gmail.com>
2026-04-12 20:37:35 +0330

    create add post button

--- frontend/src/app/(dashboard)/profile/posts/_/components/Buttons.jsx
+++ frontend/src/app/(dashboard)/profile/posts/_/components/Buttons.jsx
@@ -1,6 +1,6 @@
 "use client";
 import ButtonIcon from "@/components/ui/ButtonIcon";
-import { PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
 export function DeletePost({ id }) {
@@ -11,6 +11,19 @@ export function DeletePost({ id }) {
     >
       <TrashIcon className="text-error" />
     </ButtonIcon>
+  );
+}
+
+export function CreatePost() {
+  return (
+    <Link
+      href="/profile/posts/create"
+      className="justify-self-end flex gap-x-4 py-3 items-center rounded-lg bg-primary-900 px-4 text-sm font-medium text-secondary-0 
+      transition-colors hover:bg-primary-700"
+    >
+      <span className="hidden md:block">ایجاد پست</span>{" "}
+      <PlusIcon className="w-5" />
+    </Link>
   );
 }
 
--- frontend/src/app/(dashboard)/profile/posts/page.jsx
+++ frontend/src/app/(dashboard)/profile/posts/page.jsx
@@ -1,12 +1,19 @@
 import React, { Suspense } from "react";
 import PostsTable from "./_/components/PostsTable";
 import Fallback from "@/components/ui/Fallback";
+import Search from "@/components/ui/Search";
+import { CreatePost } from "./_/components/Buttons";
 
 function page() {
   return (
     <div>
+      <div className="grid grid-cols-1 gap-8 text-secondary-700 mb-12 items-center lg:grid-cols-3">
+        <h1 className=" font-bold text-xl ">لیست پست ها</h1>
+        <Search />
+        <CreatePost />
+      </div>
       <Suspense fallback={<Fallback />}>
-        <PostsTable />
+        <PostsTable queries="" />
       </Suspense>
     </div>
   );
